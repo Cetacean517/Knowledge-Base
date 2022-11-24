210b65726b98018548da0ae0eb093f5b2bdd88cd
Cetacean <cetacean.wang@sap.com>
2022-11-24 17:37:01 +0800

    Spring Framework

--- Java/03_SpringFramework/Code/spring-boot-learning/src/main/java/com/cetacean/springbootlearning/data/GuestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cetacean.springbootlearning.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GuestRepository extends JpaRepository<Guest,Long> {
-}
