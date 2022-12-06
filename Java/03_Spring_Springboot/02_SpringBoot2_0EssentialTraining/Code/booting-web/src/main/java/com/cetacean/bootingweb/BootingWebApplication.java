cec77a3adaa7dbed57ad0be021aade1844661192
Cetacean <cetacean.wang@sap.com>
2022-12-06 14:11:41 +0800

    Springboot

---
+++ Java/03_Spring_Springboot/02_SpringBoot2_0EssentialTraining/Code/booting-web/src/main/java/com/cetacean/bootingweb/BootingWebApplication.java
@@ -0,0 +1,13 @@
+package com.cetacean.bootingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootingWebApplication.class, args);
+	}
+
+}
